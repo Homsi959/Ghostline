192027645e16284175ae591a012610bc2805d7f8
Homsi Dark <tarek18@bk.ru>
2025-03-01 19:30:46 +0400

    Add Telegram module to app module imports

--- code/app/app.module.ts
+++ code/app/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegramModule } from 'code/telegram/telegram.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TelegramModule,
   ],
   controllers: [AppController],
   providers: [AppService],
