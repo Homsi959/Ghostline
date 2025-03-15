62b26c8fb91f98eadcf3f717c8a3e0748cab62d1
Homsi Dark <tarek18@bk.ru>
2025-03-15 13:55:18 +0300

    Add BASE_URL_3X_UI constant and update dependencies for improved functionality

--- code/common/constants.ts
+++ code/common/constants.ts
@@ -12,3 +12,8 @@ export const LOG_LEVEL_KEY = 'LOG_LEVEL_KEY';
  * Токен сервиса БД
  */
 export const DATABASE_TOKEN = 'PG_CONNECTION';
+
+/**
+ * Базовый URL для работы с прокси панелью
+ */
+export const BASE_URL_3X_UI = 'https://jsonplaceholder.typicode.com';
